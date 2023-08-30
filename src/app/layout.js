4634a961ea17402dbfb4890d3e503ca70fa020c3
Bhaskar Pathriya <bhaskarpathriya829@gmail.com>
2023-08-30 10:15:11 +0530

    Setup Next js

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'IMDb: Ratings, Reviews, and Where to Watch the Best Movies &amp; TV Shows',
+  description: 'IMDb is the worlds most popular and authoritative source for movie, TV and celebrity content. Find ratings and reviews for the newest movie and TV shows.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
